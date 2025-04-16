792e25d2ffdaf219cc6be6c5b69d5b5646a65bd6
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-04-16 17:29:24 -0600

    uncomment code for demonstartion of Set/HashSet

--- src/main/java/lemonadestand/Application.java
+++ src/main/java/lemonadestand/Application.java
@@ -17,10 +17,10 @@ public class Application {
 		order1.addLemonade(new Lemonade(1, .5, 1, 5));
 		order1.addLemonade(new Lemonade(1, .5, 1, 5));
 
-		// for (Lemonade l : order1.getLemonades()) {
-		// 	System.out.println(l.getPrice());
-		// }
-		// System.out.println(order1.getCustomer().getName() + ", your total price is: $" + order1.getTotal() + "!");
+		for (Lemonade l : order1.getLemonades()) {
+			System.out.println(l.getPrice());
+		}
+		System.out.println(order1.getCustomer().getName() + ", your total price is: $" + order1.getTotal() + "!");
 
 		order2.addLemonade(new Lemonade(1, .5, 1, 5));
 		order2.addLemonade(new Lemonade(2, .5, 1, 5));
@@ -36,22 +36,22 @@ public class Application {
 		// System.out.println(order1);
 		// System.out.println(order1.equals(order1));
 
-		Box<Order> b1 = new Box<Order>(order1);
-		Box<Customer> b2 = new Box<>(customer2);
+		// Box<Order> b1 = new Box<Order>(order1);
+		// Box<Customer> b2 = new Box<>(customer2);
 
-		b1.setObj(order2);
+		// b1.setObj(order2);
 		
 		// ((Order) b1.getObj()).addLemonade(new Lemonade(2,1.5,1.75,5));
-		b1.getObj().addLemonade(new Lemonade(2,1.5,1.75,5));
-		b2.setObj(customer1);
+		// b1.getObj().addLemonade(new Lemonade(2,1.5,1.75,5));
+		// b2.setObj(customer1);
 
 		// System.out.println(b1.getObj());
 		// System.out.println(b2.getObj());
 		// System.out.println(b2.getObj().getName());
 
-		b2.<Customer, Order>printValue(customer1, order2);
-		b2.printValue(customer1, order2);
+		// b2.<Customer, Order>printValue(customer1, order2);
+		// b2.printValue(customer1, order2);
 
-		Box<Integer> b3 = new Box<Integer>(1);
+		// Box<Integer> b3 = new Box<Integer>(1);
 	}
 }
